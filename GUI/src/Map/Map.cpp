dc3dff2ce42f18c90c34a6f08119334d51e31926
Maniko <louis.chabrerie@epitech.eu>
2024-05-28 17:07:16 +0200

    feat(GUI): add map implementation

--- GUI/src/Map/Map.cpp
+++ GUI/src/Map/Map.cpp
@@ -7,6 +7,56 @@
 
 #include "Map.hpp"
 
-zappy::Map::Map() = default;
+zappy::Map::Map(std::size_t width, std::size_t height)
+{
+    for (std::size_t i = 0; i < width; i++) {
+        _map.emplace_back();
+        for (std::size_t j = 0; j < height; j++) {
+            _map[i].emplace_back();
+        }
+    }
+}
 
 zappy::Map::~Map() = default;
+
+zappy::Box &zappy::Map::operator()(int x, int y)
+{
+    if (x < 0 || y < 0 || x >= _map.size() || y >= _map[0].size())
+        throw Map::MapError("Out of bounds");
+
+    return _map[x][y];
+}
+
+std::shared_ptr<zappy::Trantorien> zappy::Map::getPlayerById(std::size_t id)
+{
+    for (auto &player : _players) {
+        if (player->id == id)
+            return player;
+    }
+
+    throw Map::MapError("Player not found");
+}
+
+void zappy::Map::addPlayer(std::shared_ptr<Trantorien> player)
+{
+    _players.push_back(player);
+}
+
+void zappy::Map::removePlayerById(std::size_t id)
+{
+    std::shared_ptr<Trantorien> player = getPlayerById(id);
+
+    if (player != nullptr) {
+        _players.erase(_players.begin() + _getPlayerIndexById(id));
+    }
+}
+
+std::size_t zappy::Map::_getPlayerIndexById(std::size_t id)
+{
+    for (std::size_t i = 0; i < _players.size(); i++) {
+        if (_players[i]->id == id)
+            return i;
+    }
+
+    throw Map::MapError("Player not found");
+}
--- GUI/src/Map/Map.hpp
+++ GUI/src/Map/Map.hpp
@@ -7,17 +7,35 @@
 
 #pragma once
 
+#include <string>
+#include "Box.hpp"
+
 namespace zappy
 {
     class Map
     {
         public:
-            Map();
+            Map(std::size_t width = 10, std::size_t height = 10);
 
             ~Map();
 
-            void operator()(int x, int y) const;
+            class MapError : public std::exception
+            {
+                public:
+                    MapError(std::string const &message) : _message(message) {}
+                    const char *what() const noexcept override { return _message.c_str(); }
+                private:
+                    std::string _message;
+            };
+
+             Box &operator()(int x, int y);
+             std::shared_ptr<Trantorien> getPlayerById(std::size_t id);
+             void addPlayer(std::shared_ptr<Trantorien> player);
+             void removePlayerById(std::size_t id);
 
         private:
+            std::vector<std::vector<Box>> _map;
+            std::vector<std::shared_ptr<Trantorien>> _players = {};
+            std::size_t _getPlayerIndexById(std::size_t id);
     };
 }
