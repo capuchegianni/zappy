17206b83a47500ebc0cfc662e08f46aa12db5f11
Maniko <louis.chabrerie@epitech.eu>
2024-05-28 11:07:01 +0200

    build(GUI): CMake config

---
+++ GUI/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** GUI
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    std::cout << "Hello World from GUI!" << std::endl;
+
+    return 0;
+}
