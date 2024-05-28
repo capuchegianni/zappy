d91239c4c6333d40c24dd647e36ee73d23928dc7
Maniko <louis.chabrerie@epitech.eu>
2024-05-28 17:47:31 +0200

    feat(GUI): add player gestion in box

--- GUI/src/Map/Box.cpp
+++ GUI/src/Map/Box.cpp
@@ -10,3 +10,28 @@
 zappy::Box::Box() = default;
 
 zappy::Box::~Box() = default;
+
+void zappy::Box::addPlayer(std::shared_ptr<Trantorien> player)
+{
+    _players.push_back(player);
+}
+
+void zappy::Box::removePlayerById(std::size_t id)
+{
+    std::size_t index = _getPlayerIndexById(id);
+
+    _players.erase(_players.begin() + index);
+
+    if (_players.empty())
+        throw Box::BoxError("No more players in this box");
+}
+
+std::size_t zappy::Box::_getPlayerIndexById(std::size_t id)
+{
+    for (std::size_t i = 0; i < _players.size(); i++) {
+        if (_players[i]->id == id)
+            return i;
+    }
+
+    throw Box::BoxError("Player not found");
+}
--- GUI/src/Map/Box.hpp
+++ GUI/src/Map/Box.hpp
@@ -10,6 +10,7 @@
 #include <cstddef>
 #include <vector>
 #include <memory>
+#include <string>
 #include "../Characters/Trantorien.hpp"
 
 namespace zappy
@@ -18,8 +19,16 @@ namespace zappy
     {
         public:
             Box();
-
             ~Box();
+
+            class BoxError : public std::exception
+            {
+                public:
+                    BoxError(std::string const &message) : _message(message) {}
+                    const char *what() const noexcept override { return _message.c_str(); }
+                private:
+                    std::string _message;
+            };
 
             std::size_t food = 0;
             std::size_t linemate = 0;
@@ -29,6 +38,10 @@ namespace zappy
             std::size_t phiras = 0;
             std::size_t thystame = 0;
 
-            std::vector<std::shared_ptr<Trantorien>> players = {};
+            void addPlayer(std::shared_ptr<Trantorien> player);
+            void removePlayerById(std::size_t id);
+        private:
+            std::vector<std::shared_ptr<Trantorien>> _players = {};
+            std::size_t _getPlayerIndexById(std::size_t id);
     };
 }
