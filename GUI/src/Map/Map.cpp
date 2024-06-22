1940dc6b172e58276d544f0132634593fbdb248e
Maniko <louis.chabrerie@epitech.eu>
2024-06-22 16:26:52 +0200

    fix(GUI): selection frame display above everything

--- GUI/src/Map/Map.cpp
+++ GUI/src/Map/Map.cpp
@@ -164,16 +164,16 @@ void zappy::Map::updateTiles()
             sceneDate.renderTexture.draw(*box);
         }
     }
-}
 
-void zappy::Map::updateDirectionUI()
-{
     sceneDate.selection.setTexture(sceneDate.sceneData.selectionTile.getTexture(), true);
     sceneDate.selection.setOrigin(sceneDate.selection.getTexture()->getSize().x / 2, sceneDate.selection.getTexture()->getSize().y / 2);
     sceneDate.selection.setPosition(sceneDate.sceneData.camera.displayUnitaryX.x * selectedBox.x + sceneDate.sceneData.camera.displayUnitaryY.x * selectedBox.y + sceneDate.sceneData.camera.centerX * sceneDate.sceneData.camera.displayUnitaryX.x + sceneDate.sceneData.camera.displayUnitaryY.x * sceneDate.sceneData.camera.centerY + sceneDate.sceneData.camera.displayUnitaryZ.x * sceneDate.sceneData.camera.centerZ,
                                     sceneDate.sceneData.camera.displayUnitaryX.y * selectedBox.x + sceneDate.sceneData.camera.displayUnitaryY.y * selectedBox.y + sceneDate.sceneData.camera.centerX * sceneDate.sceneData.camera.displayUnitaryX.y + sceneDate.sceneData.camera.displayUnitaryY.y * sceneDate.sceneData.camera.centerY + sceneDate.sceneData.camera.displayUnitaryZ.y * sceneDate.sceneData.camera.centerZ);
     sceneDate.renderTexture.draw(sceneDate.selection);
-    
+}
+
+void zappy::Map::updateDirectionUI()
+{
     sceneDate.compass.setTexture(sceneDate.sceneData.compassTile.getTexture(), true);
     sceneDate.compass.setOrigin(sceneDate.compass.getTexture()->getSize().x / 2, sceneDate.compass.getTexture()->getSize().y / 2);
     sceneDate.compass.setPosition(80-sceneDate.rect.getSize().x / 2, 80-sceneDate.rect.getSize().y / 2);
